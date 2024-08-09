3928d59bd249136c41f96b786e5afb629c4b7ba9
mohamed abdelwahab <101339472+muhamedmahmud@users.noreply.github.com>
2024-08-09 09:18:17 +0300

    bmicalculator

---
+++ script.js
@@ -0,0 +1,25 @@
+// script.js
+function calculateBMI() {
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    if (weight > 0 && height > 0) {
+        const heightInMeters = height / 100;
+        const bmi = weight / (heightInMeters * heightInMeters);
+        let category = '';
+
+        if (bmi < 18.5) {
+            category = 'Underweight';
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            category = 'Normal weight';
+        } else if (bmi >= 25 && bmi < 29.9) {
+            category = 'Overweight';
+        } else {
+            category = 'Obesity';
+        }
+
+        document.getElementById('bmiResult').innerHTML = `Your BMI is ${bmi.toFixed(2)}. You are classified as ${category}.`;
+    } else {
+        document.getElementById('bmiResult').innerHTML = 'Please enter valid values for weight and height.';
+    }
+}
